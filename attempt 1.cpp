2c2f5c87f3c9e45933f032a0a05ca92183b93d63
GetChecked <allancmartin@ufl.edu>
2015-04-18 19:50:09 -0400

    checking cin of board locations

--- attempt 1.cpp
+++ attempt 1.cpp
@@ -1,13 +1,33 @@
 //Allan Martin attempt 1 checkers
 
-
 #include <iostream>
-#include <map>
 
 using namespace std;
 
+
+
 int main()
 {
-	map <char, int> blackpiecesMap;
-	//bla bla bla no idea whats going on
+	char board[8][8];
+	char row1 = 0;
+	int column1 = 0;
+	cout << "Select current piece location (ex -> A,4): \n";
+	cin >> board[row1][column1];
+	row1 -= 65;
+
+	//playerMove(Player);
+
+	system("PAUSE");
+	return 0;
 }
+
+/*void playerMove(char turn)
+{
+	if (turn == Player1)
+	{
+		cout << "Select current piece location (ex -> A 4): \n";
+		cin >> board[row1][column1];
+		if (row1 column)
+
+	}
+}*/
